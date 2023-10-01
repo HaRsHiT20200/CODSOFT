265cac8d653b2767cb72f224dd3ab31d16f41d40
HaRsHiT20200 <harshitpandey20112004@gmail.com>
2023-10-01 23:03:47 +0530

    password_generator

--- calculator.py
+++ calculator.py
@@ -1,19 +1,34 @@
 print("********* Basic calculator**********")
+
 print("\nEnter # as operator for exit\n")
+
+# loop for performing unlimited operations 
 while True:
+
+    #get numbers as num1 and num2
     num1=int(input("1st Value: "))
+
     num2=int(input("2st Value: "))
-    print("Choose an operator: +  -  *  /")
-    op=input()
+
+    # get operator from user
+    
+    op=input("Choose an operator: +  -  *  /")
+
+    
     if op=="+":
         print(f"{num1}+{num2}=",num1+num2)
+    
     elif op=="-":
         print(f"{num1}-{num2}=",num1-num2)
+    
     elif op=="*":
         print(f"{num1}*{num2}=",num1*num2)
+    
     elif op=="/":
         print(f"{num1}/{num2}=",num1/num2)
+    
     elif op=="#":
         break
+    
     else:
         print("Enter valid operator")
---
+++ password_generator.py
@@ -0,0 +1,54 @@
+import random
+import string
+
+print("****** Password Generator ******")
+
+s = ""
+s1 = string.ascii_uppercase
+s2 = string.ascii_lowercase
+s3 = string.digits
+s4 = "!@#$%&*_?"
+
+loop=True
+while loop:
+    print("\nChoose difficulty level")
+    print("1: Easy")
+    print("2: Moderate")
+    print("3: Hard")
+
+    level = int(input("Level: "))
+    if level==1:
+        s = s1 + s2
+        length = int(input("Enter length of password"))
+        if length==0:
+            break
+        password = "".join(random.sample(s,length))
+        print(f"Your password is: {password}")  
+
+
+    elif level==2:
+        s = s1 + s2 + s3
+        length = int(input("Enter length of password"))
+        if length==0:
+            break
+        password = "".join(random.sample(s,length))
+        print(f"Your password is: {password}")  
+
+    elif level==3:
+        s = s1 + s2 + s3 + s4
+        length = int(input("Enter length of password"))
+        if length==0:
+            break
+        password = "".join(random.sample(s,length))
+        print(f"Your password is: {password}")
+
+    else:
+        print("Level not found. Try again")
+      
+
+
+
+
+
+
+
